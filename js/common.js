e1dce64770fa422e5826658234f72dc654ef913a
megal <wmmegal@gmail.com>
2020-09-16 18:41:09 +0300

    new

--- js/common.js
+++ js/common.js
@@ -43,7 +43,7 @@ jQuery(document).ready(function ($) {
   /**
    * Autocomplete form
    */
-  const autocompleteInput = $('.input-autocomplete input');
+  const autocompleteInput = $('.input-autocomplete input, .quick-search-form input');
 
   autocompleteInput.on('blur', function () {
     let $this = $(this);
@@ -69,5 +69,48 @@ jQuery(document).ready(function ($) {
   $(document).on('click', '#add-review .search-results li', function (e) {
     autocompleteInput.val($(this).data('title'));
     $('[name=post_id]').val($(this).data('id'));
+  });
+
+  /**
+   * Дроп списки
+   */
+  $(document).on('click', '.btn-drop-toggle', function () {
+    const $this = $(this);
+    $this.toggleClass('active');
+    $this.closest('div').siblings().find('.btn-drop-toggle, .drop-list').removeClass('active');
+    $this.closest('div').find('.drop-list').toggleClass('active');
+  });
+
+  $(document).on('click input', function (event) {
+    if (check_drop_list($(event.target))) return;
+    $('.drop-list').removeClass('active');
+    $('.btn-drop-toggle').removeClass('active');
+    event.stopPropagation();
+  });
+
+  function check_drop_list(el) {
+    return el.hasClass("btn-drop-toggle") || el.closest('.btn-drop-toggle').length || el.is('input') || el.closest('.drop-list').length;
+  }
+
+  /**
+   * Скрыть списко после выбора в фильтре
+   */
+  $('.filter-select .drop-list .checkbox').on('click', function () {
+    const $this        = $(this),
+          $thisVal     = $this.find('span').text(),
+          filterSelect = $this.closest('.filter-select');
+
+    if (filterSelect.hasClass('multiselect')) {
+
+    } else {
+      filterSelect.find('.selected-value').text($thisVal);
+      filterSelect.find('.drop-list, .btn-drop-toggle').removeClass('active');
+    }
+  });
+
+  $('.filter-select input[type=text]').on('input', function () {
+    const $this = $(this);
+    let val = $this.val() ? $this.val() + ' руб' : 'Любая';
+    $this.closest('.filter-select').find('.selected-value').text(val);
   });
 });
